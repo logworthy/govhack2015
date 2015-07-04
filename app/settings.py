4da1ddd618c10e7d2b746b43246b1dbabe70f705
Lex Toumbourou <lextoumbourou@gmail.com>
2015-07-04 18:41:32 +1000

    Updated settings.

--- app/settings.py
+++ app/settings.py
@@ -77,4 +77,7 @@ USE_TZ = True
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "app/assets"),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATIC_URL = '/static/'
--- fabfile.py
+++ fabfile.py
@@ -11,4 +11,5 @@ def deploy():
     run("cd /srv/govhack2015 && git pull origin master")
     run("/srv/govhack2015/ENV/bin/pip install -r /srv/govhack2015/requirements.txt")
     run("cd /srv/govhack2015 && /srv/govhack2015/ENV/bin/python manage.py migrate")
+    run("cd /srv/govhack2015 && /srv/govhack2015/ENV/bin/python manage.py collectstatic")
     run("sudo supervisorctl restart govhack2015")

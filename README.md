521ac2c64039918e5a7f490ef5b9423094e67a20
Lex Toumbourou <lextoumbourou@gmail.com>
2015-07-04 10:23:55 +1000

    Init.

---
+++ README.md
@@ -0,0 +1,12 @@
+Govhack 2015: Regional News Explorer (tentative title)
+======================================================
+
+## Server setup (Debian-based OS)
+
+```
+> apt-get update
+> apt-get install -y postgresql postgresql-contrib
+> apt-get install -y postgis postgresql-9.3-postgis-2.1
+> createdb local_explorer
+> sudo -u postgres psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" local_explorer
+```
---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.gis.db.models import PointField
+from django.contrib.postgres.fields import ArrayField
+
+
+class Story(models.Model):
+    title = models.CharField()
+    url = models.URLField()
+    date = models.DateField()
+    primary_image = models.URLField()
+    primary_image_caption = models.CharField()
+    primary_image_rights_information = models.CharField()
+    subjects = ArrayField()
+    station = models.CharField()
+    state = models.CharField()
+    place = models.CharField()
+    keywords = ArrayField()
+    location = PointField()
